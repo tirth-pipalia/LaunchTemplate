31235e5a8d2cfda05aa9569e5fc43f5056af5d3d
tirth pipalia <56732761+tirth-pipalia@users.noreply.github.com>
2022-04-04 17:33:20 +0100

    Update app.py

--- LaunchTemplate/app.py
+++ LaunchTemplate/app.py
@@ -7,7 +7,7 @@ from launch_template.launch_template_stack import LaunchTemplateStack
 
 
 app = cdk.App()
-env_ME = cdk.Environment(account="620228650709", region="me-south-1")
+env_ME = cdk.Environment(account="12_digit_account_no", region="me-south-1")
 LaunchTemplateStack(app, "LaunchTemplateStack", env=env_ME)
 
 app.synth()
